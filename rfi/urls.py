badaeaa467a77dd1a9943ee94482254133c030bd
Marc Pfister <marc.pfister@gmail.com>
2012-08-19 19:30:30 -0600

    add info window view

--- rfi/urls.py
+++ rfi/urls.py
@@ -7,6 +7,7 @@ entry_resource = RFIResource()
 urlpatterns = patterns('',
         (r'^api/', include(entry_resource.urls)),
         (r'^request/(\d+)/$', 'rfi.views.request_info'),
+        (r'^info/(\d+)/$', 'rfi.views.info_window'),
         (r'^new/$', 'rfi.views.new_request'),
         (r'^complete/$', 'rfi.views.complete'),
         )
--- rfi/views.py
+++ rfi/views.py
@@ -1,5 +1,5 @@
 from rfi.models import RequestForImagery, RequestForm
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render, render_to_response, get_object_or_404
 from django.http import HttpResponseRedirect
 
 def request_info(request, pk):
@@ -8,6 +8,10 @@ def request_info(request, pk):
     except RequestForImagery.DoesNotExist:
         return render_to_response('rfi/missing.html')
     return render_to_response('rfi/info_request.html', {'rfi': rfi})
+
+def info_window(request, pk):
+    rfi = get_object_or_404(RequestForImagery, pk=int(pk))
+    return render_to_response('rfi/info_window.html', {'rfi': rfi})
 
 def new_request(request):
     if request.method == 'POST':
